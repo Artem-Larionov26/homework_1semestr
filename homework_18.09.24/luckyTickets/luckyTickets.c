9b6657bb9cfabc018cd1636daa98d023ce81caa6
homework_1sem <st129909@student.spbu.ru>
2024-10-08 13:09:11 +0300

    add two task

---
+++ homework_18.09.24/luckyTickets/luckyTickets.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+void calculate_sums(int numbers[28]) {
+	for (int i = 0; i <= 9; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = 0; k <= 9; k++) {
+				int sum = i + j + k;
+				numbers[sum]++;
+			}
+		}
+	}
+}
+
