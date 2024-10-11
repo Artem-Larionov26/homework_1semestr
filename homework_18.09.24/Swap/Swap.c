6c7f25439125e3ed04fa24bb3ba204d41b4267e4
homework_1sem <st129909@student.spbu.ru>
2024-10-11 03:11:28 +0300

    feat: add an example

---
+++ homework_18.09.24/Swap/Swap.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void swap(int* right, int* left) {
+	if (right != left) {
+		*left ^= *right;
+		*right ^= *left;
+		*left ^= *right;
+	}
+}
+
+void twoVariants() {
+	int a = 5, b = 3;
+	swap(&a, &b);
+	printf("The original version: a = 5, b = 3\nResult: a = %d, b = %d\n", a, b);
+}
+
+int main(void) {
+	twoVariants();
+}
