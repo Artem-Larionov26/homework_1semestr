12c5265a22795e68d56d572171e4d4527c67b56e
homework_1sem <st129909@student.spbu.ru>
2024-10-24 03:41:37 +0300

    add a division by zero check

---
+++ homework_18.09.24/Incomplete quotient/Incomplete quotient.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int incompleteQuotient(int a, int b) {
+	if (b == 0) {
+		printf("Ошибка: деление на ноль!\n");
+		exit(EXIT_FAILURE);
+	}
+	int quotient = 0;
+	int negativeResult = 0;
+	if (a < 0) {
+		a = -a;
+		negativeResult = !negativeResult;
+	}
+	if (b < 0) {
+		b = -b;
+		negativeResult = !negativeResult;
+	}
+	while (a >= b) {
+		a = a - b;
+		quotient++;
+	}
+	if (negativeResult) {
+		quotient = -quotient;
+	}
+	return quotient;
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int result = incompleteQuotient(18, -8);
+	printf("Неполное частное: %d\n", result);
+}
