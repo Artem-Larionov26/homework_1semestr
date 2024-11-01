f8eb91b1a0edab441a0dbe538462f96e0f644186
homework_1sem <st129909@student.spbu.ru>
2024-11-01 16:25:00 +0300

    added a check

---
+++ controlWork_01.11.24/task1/task1.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+bool Palindrome(int num) {
+	if (num < 0) {
+		return false;
+	}
+	int reversed = 0;
+	int original = num;
+	while (num != 0) {
+		int digit = num % 10;
+		reversed = reversed * 10 + digit;
+		num /= 10;
+	}
+	return original == reversed;
+}
+
+int main() {
+	int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	if (Palindrome(num)) {
+		printf("The number %d is a palindrome \n", num);
+	} else {
+		printf("The number %d isn't a palindrome \n", num);
+	}
+	return 0;
+}
