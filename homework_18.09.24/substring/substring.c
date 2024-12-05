ebf926d1fdbe8236fc9cb0f22b846638f7fd5a31
homework_1sem <st129909@student.spbu.ru>
2024-12-06 02:57:40 +0300

    feat: fixed the tests and added user interaction

--- homework_18.09.24/substring/substring.c
+++ homework_18.09.24/substring/substring.c
@@ -1,19 +1,19 @@
-﻿#include<stdio.h>
-#include<string.h>
+﻿#include <stdio.h>
+#include <string.h>
 
-int numberOfOccurrences(const char* S, const char* S1) {
+int numberOfOccurrences(const char* s, const char* s1) {
 	int counter = 0;
-	int lenS = strlen(S);
-	int lenS1 = strlen(S1);
+	int lenS = strlen(s);
+	int lenS1 = strlen(s1);
 	for (int i = 0; i <= lenS - lenS1; i++) {
 		int flag = 1;
 		for (int j = 0; j < lenS1; j++) {
-			if (S[i + j] != S1[j]) {
+			if (s[i + j] != s1[j]) {
 				flag = 0;
 				break;
 			}
 		}
-		if (flag) {
+		if (flag == 1) {
 			counter++;
 		}
 	}
@@ -21,27 +21,54 @@ int numberOfOccurrences(const char* S, const char* S1) {
 }
 
 void runTests() {
-	printf("Test 1: %s in %s : %d\n", "abc", "abcabcabc", numberOfOccurrences("abcabcabc", "abc"));
-	printf("Expected : 3\n");
-	printf("Test 2: %s in %s : %d\n", "lo", "hellohellohello", numberOfOccurrences("hellohellohello", "lo"));
-	printf("Expected : 3\n");
-	printf("Test 3: %s in %s : %d\n", "aa", "aaaaaa", numberOfOccurrences("aaaaaa", "aa"));
-	printf("Expected : 5\n");
-	printf("Test 4: %s in %s : %d\n", "e", "abcd", numberOfOccurrences("abcd", "e"));
-	printf("Expected : 0\n");
-	printf("Test 5: %s in %s : %d\n", "abcd", "abcdabcd", numberOfOccurrences("abcdabcd", "abcd"));
-	printf("Expected : 2\n");
-	printf("Test 6: %s in %s : %d\n", "ab", "abbaaaababbbab", numberOfOccurrences("abbaaaababbbab", "ab"));
-	printf("Expected : 4\n");
-	printf("Test 7: %s in %s : %d\n", "a", "", numberOfOccurrences("", "a"));
-	printf("Expected : 0\n");
+	if (numberOfOccurrences("abcabcabc", "abc") == 3) {
+		printf("Test 1 passed!\n");
+	} else {
+		printf("Test 1 failed!\n");
+	}
+	if (numberOfOccurrences("hellohellohello", "lo") == 3) {
+		printf("Test 2 passed!\n");
+	} else {
+		printf("Test 2 failed!\n");
+	}
+	if (numberOfOccurrences("aaaaaa", "aa") == 5) {
+		printf("Test 3 passed!\n");
+	} else {
+		printf("Test 3 failed!\n");
+	}
+	if (numberOfOccurrences("abcd", "e") == 0) {
+		printf("Test 4 passed!\n");
+	} else {
+		printf("Test 4 failed!\n");
+	}
+	if (numberOfOccurrences("abcdabcd", "abcd") == 2) {
+		printf("Test 5 passed!\n");
+	} else {
+		printf("Test 5 failed!\n");
+	}
+	if (numberOfOccurrences("abbaaaababbbab", "ab") == 4) {
+		printf("Test 6 passed!\n");
+	} else {
+		printf("Test 6 failed!\n");
+	}
+	if (numberOfOccurrences("", "a") == 0) {
+		printf("Test 7 passed!\n");
+	} else {
+		printf("Test 7 failed!\n");
+	}
 }
 
 int main() {
-	char S[] = { "abcabcabcabc" };
-	char S1[] = { "abc" };
-	int occurrences = numberOfOccurrences(S, S1);
-	printf("The number of occurrences of the string %s in string %s : %d\n", S1, S, occurrences);
 	runTests();
+	char s[100];
+	char s1[100];
+	printf("Enter the main string: ");
+	fgets(s, sizeof(s), stdin);
+	s[strcspn(s, "\n")] = '\0';
+	printf("Enter the substring: ");
+	fgets(s1, sizeof(s1), stdin);
+	s1[strcspn(s1, "\n")] = '\0';
+	int occurrences = numberOfOccurrences(s, s1);
+	printf("The number of occurrences of the string %s in string %s : %d\n", s1, s, occurrences);
 	return 0;
 }
