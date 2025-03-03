547ba9707ba2403f6c1a5fb753582703a596addd
homework_1sem <st129909@student.spbu.ru>
2025-03-04 02:15:13 +0300

    feat: add tests

---
+++ stack/stack.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "stack.h"
+
+void push(Stack* stack, int value) {
+    StackElement* element = malloc(sizeof(StackElement));
+    if (element == NULL) {
+        return;
+    }
+    element->value = value;
+    element->next = stack->head;
+    stack->head = element;
+}
+
+void pop(Stack* stack) {
+    if (stack->head == NULL) {
+        return;
+    }
+    StackElement* tmp = stack->head;
+    stack->head = stack->head->next;
+    free(tmp);
+}
+
+Stack* createStack() {
+    return (Stack*)calloc(1, sizeof(Stack));
+}
+
+bool isEmpty(Stack* stack) {
+    return stack->head == NULL;
+}
