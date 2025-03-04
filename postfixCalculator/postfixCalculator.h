a861d74f15bb027b8c78b1e508bdd37d9513ccc6
homework_1sem <st129909@student.spbu.ru>
2025-03-04 12:56:05 +0300

    fix: added comments to the functions

--- postfixCalculator/postfixCalculator.h
+++ postfixCalculator/postfixCalculator.h
@@ -3,8 +3,11 @@
 #include <stdbool.h>
 #include "stack.h"
 
+// Function that checks whether an element is a number
 bool isDigit(char element);
 
+// Function that takes elements from the stack
 void takeElementsFromStack(Stack* stack, int* number1, int* number2, int* numberOfElementsInTheStack);
 
+// Function that evaluates an expression in reverse Polish notation
 int postfixCalculator(char* string, int* errorCode);
