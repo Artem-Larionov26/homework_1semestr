99875151dbc366d50bf0698fcca8aea2e15f096c
homework_1sem <st129909@student.spbu.ru>
2025-03-05 19:23:23 +0300

    feat: add tests

---
+++ rewritingTask2/insertionSort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Function that sorts array elements with even indexes by inserts
+void insertionSortEvenIndices(int array[], int size);
+
+// Function that outputs an array
+void printArray(int array[], int size);
---
+++ rewritingTask2/main.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "insertionSort.h"
+#include "tests.h"
+
+int main() {
+    if (!allTests()) {
+        printf("Tests failed!\n");
+        return -1;
+    }
+    int size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int* array = malloc(size * sizeof(int));
+    printf("Enter %d of the array elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+    insertionSortEvenIndices(array, size);
+    printf("Array after sorting even indexes:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
