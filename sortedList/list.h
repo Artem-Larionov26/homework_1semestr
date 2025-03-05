314c852d4cc00f4ba89053a131a723d8a875cfa8
homework_1sem <st129909@student.spbu.ru>
2025-03-05 03:03:43 +0300

    feat: add tests

---
+++ sortedList/list.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct ListElement ListElement;
+
+typedef struct List List;
+
+typedef ListElement* Position;
+
+typedef int Value;
+
+// Create a list
+List* createList(int* errorCode);
+
+// Position of the first element
+Position firstElement(List* list);
+
+// Function that adds an element after a position
+int addValue(List* list, Position position, int value, int* errorCode);
+
+// Function that adds an item to the head of the list
+void addInHead(List* list, int value, int* errorCode);
+
+// function that adds an item to the tail of the list
+void addInTail(List* list, int value, int* errorCode);
+
+// Function that deletes an element after a position
+void deleteElement(List* list, Position position);
+
+// Returns an element by index
+Position getElement(List* list, int index);
+
+// Function that returns the name and phone number of an element
+Value getValue(List* list, Position position);
+
+// Function that checks whether the element is the last one
+bool isLast(List* list, Position position);
+
+// Function that checks whether the position is valid
+bool isValid(List* list, Position position);
+
+// Getting the next element
+Position next(Position position);
+
+// Returns a list item
+Value getSizeList(List* list);
+
+// Function that deletes the list
+void deleteList(List* list);
---
+++ sortedList/main.c
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include "list.h"
+#include "testsList.h"
+
+int main() {
+    if (!testList()) {
+        printf("Test falied!\n");
+        return 1;
+    }
+    int errorCode = 0;
+    List* list = createList(&errorCode);
+    int operationNumber = 7;
+    if (errorCode == 1) {
+        printf("Memory allocation error!\n");
+        return 1;
+    }
+    while (operationNumber != 0) {
+        printf("Enter operation number: ");
+        scanf("%d", &operationNumber);
+        if (operationNumber == 0) {
+            deleteList(list);
+            return 0;
+        }
+        else if (operationNumber == 1) {
+            int value = 0;
+            printf("Enter value: ");
+            scanf("%d", &value);
+            Position position = firstElement(list);
+            if (getSizeList(list) == 0) {
+                addInTail(list, value, &errorCode);
+                if (errorCode == 1) {
+                    printf("Memory allocation error!\n");
+                    return 1;
+                }
+            }
+            else {
+                while (getValue(list, position) < value) {
+                    position = next(position);
+                }
+                addValue(list, position, value, &errorCode);
+                if (errorCode == 1) {
+                    printf("Memory allocation error!\n");
+                    return 1;
+                }
+            }
+        }
+        else if (operationNumber == 2) {
+            if (getSizeList(list) > 0) {
+                deleteElement(list, firstElement(list));
+            }
+            else {
+                printf(" There are no elements in the array!\n");
+            }
+        }
+        else if (operationNumber == 3) {
+            printf("Sorted list:\n");
+            if (getSizeList(list) == 0) {
+                printf(" The list is empty!\n");
+            }
+            else {
+                for (Position i = firstElement(list); !isLast(list, i); i = next(i)) {
+                    printf("%d ", getValue(list, i));
+                }
+                printf("\n");
+            }
+        }
+    }
+}
